e0a5c74d4523585df60887e50b40dbd161cd231c
Sabir5911 <sabirali786692@gmail.com>
2023-09-29 18:46:16 +0500

    redux

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -3,44 +3,44 @@ import Cart from '../components/Cart'
 import {  User} from '../lib/drizzle';
 import { cookies } from 'next/dist/client/components/headers';
 
-const BASE_URL =
-  process.env.NODE_ENV == "development"
-    ? "http://localhost:3000"
-    : "e-com-restored.vercel.app";
-   ;
+// const BASE_URL =
+//   process.env.NODE_ENV == "development"
+//     ? "http://localhost:3000"
+//     : "e-com-restored.vercel.app";
+//    ;
 
- async function GetData() {
+//  async function GetData() {
     
-    try {
-        const res = await fetch(`http://localhost:3000/api`, {
-          headers: {
-            "Content-type": "application/json",
-          },
-          method: "GET",
-          cache: "no-store",
-        });
+//     try {
+//         const res = await fetch(`http://localhost:3000/api`, {
+//           headers: {
+//             "Content-type": "application/json",
+//           },
+//           method: "GET",
+//           cache: "no-store",
+//         });
     
-        if (!res.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        const result = await res.json();
-    console.log(result);
+//         if (!res.ok) {
+//           throw new Error("Failed to fetch data");
+//         }
+//         const result = await res.json();
+//     console.log(result);
     
-        return result;
-      } catch (error) {
-        console.log((error as { message: string }).message);
-      }
-}
+//         return result;
+//       } catch (error) {
+//         console.log((error as { message: string }).message);
+//       }
+// }
 
 
 export default async function page() {
         
 
-  const data:User[]=await GetData()
+  // const data:User[]=await GetData()
   
-  console.log(data,'datatatattata');
+  // console.log(data,'datatatattata');
   
-  const cartdata = data .filter( (elm) => elm.user_id == cookies().get("user_id")?.value as string  );
+  // const cartdata = data .filter( (elm) => elm.user_id == cookies().get("user_id")?.value as string  );
 
                   
   return (
