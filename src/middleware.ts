34629b7a46cc133b620e5d1cbe5c0a3760ea31be
Sabir5911 <sabirali786692@gmail.com>
2023-09-29 17:24:58 +0500

    comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
     publicRoutes:['/',"/Products/Female","/Products/Male","/Products/Female","/api:path*","/cart","/Products/AllProducts","/Products/Kids"],
-    ignoredRoutes:["/((?!api|trpc))(_next.*|.+\.[\w]+$)"],
+    // ignoredRoutes:["/((?!api|trpc))(_next.*|.+\.[\w]+$)"],
 
 });
  
